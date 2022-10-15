89f305861ad911e44c3bf5f95b24e0d508163a2c
Raoul Zebisch <neroware@freenet.de>
2022-10-15 19:00:28 +0200

    Added dummy enemy sprite

---
+++ scenes/features/enemies/src_enemy_spawner.gd
@@ -0,0 +1,16 @@
+extends Node2D
+class_name Spawner
+
+@export var enabled : bool = true
+@export var spawned_scene : PackedScene = preload("res://scenes/features/enemies/src_dummy_enemy.tscn")
+
+@export @onready var root_node : Node = $".."
+
+var spawned_entity : Node = null
+
+func _process(delta):
+	if enabled and spawned_entity == null:
+		spawned_entity = spawned_scene.instantiate()
+		spawned_entity.name = "SpawnedEntity" + str(spawned_entity.get_instance_id())
+		spawned_entity.position = self.position
+		root_node.add_child(spawned_entity)
